ed0dc32db80c21d3a951fd472685ca4336c83aec
João Caçote <ctw02273@criticaltechworks.com>
2022-07-20 14:47:03 +0100

    all done

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,6 +74,18 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "Grafana"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create network interface
