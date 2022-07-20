d13a0833ff49aab47c6428fee71c60099bcdb4a3
João Caçote <ctw02273@criticaltechworks.com>
2022-07-20 12:20:35 +0100

    prometheus feature

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,6 +62,18 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "Prometheus"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9090"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 # Create network interface
